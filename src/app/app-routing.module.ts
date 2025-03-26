879b76dd5a5550a94b6674cb683471e304aa8172
FoodForPowder <zaharkin.artur@yandex.ru>
2025-03-26 21:32:46 +0700

    added item lazyLoad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageModule } from './page/page.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '/item',
+    loadChildren: () => import('./page/page.module').then((m) => m.PageModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
