64c81963420bc1505a868912a717894e01995578
FoodForPowder <zaharkin.artur@yandex.ru>
2025-03-27 22:45:22 +0700

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PageModule } from './page/page.module';
+import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
   {
-    path: '/item',
+    path: 'item',
     loadChildren: () => import('./page/page.module').then((m) => m.PageModule),
   },
+  { path: 'main', component: MainComponent },
 ];
 
 @NgModule({
