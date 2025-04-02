61530a3d49625b46676e9195f624120f5bc4d0af
FoodForPowder <zaharkin.artur@yandex.ru>
2025-04-02 19:31:17 +0700

    added ol

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import Map from 'ol/Map.js';
+import View from 'ol/View.js';
+import TileLayer from 'ol/layer/Tile.js';
+import OSM from 'ol/source/OSM.js';
+import { fromLonLat } from 'ol/proj';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'EvoCourse';
+  map!: Map;
+  count = 0;
   constructor(private toastr: ToastrService) {}
+  ngOnInit(): void {
+    this.map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      view: new View({
+        center: fromLonLat([37.540441, 55.748045]),
+        zoom: 19,
+      }),
+    });
+  }
 
   showSuccess() {
-    this.toastr.success('Hello world!', 'Toastr fun!');
+    this.toastr.success(`Это ваше ${++this.count} нажатие!`, 'Кнопка нажата!');
   }
 }
