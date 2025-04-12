8e7190e72170cc9b6c8183f99b2f99159a3c3074
FoodForPowder <zaharkin.artur@yandex.ru>
2025-04-12 14:16:31 +0700

    added task 2.3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { interval, map, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'EvoCourse';
+  numbers: number[] = [];
+  randomNumbers: string[] = [];
+
+  numberSubscription$!: Subscription;
+  randomNumberSubscription$!: Subscription;
+
+  ngOnInit() {
+    this.initStreams();
+  }
+  initStreams() {
+    const nubmerInterval$ = interval(2000);
+    this.numberSubscription$ = nubmerInterval$.subscribe((number) => {
+      this.numbers = [...this.numbers, number];
+    });
+    this.randomNumberSubscription$ = nubmerInterval$
+      .pipe(
+        map(() => Math.floor(Math.random() * 100)),
+        map((num) => `Random Value: ${num}`)
+      )
+      .subscribe((number) => {
+        this.randomNumbers = [...this.randomNumbers, number];
+      });
+  }
+  numberSubscriptionUnsubscribe() {
+    this.numberSubscription$.unsubscribe();
+  }
+  randomNumberSubscriptionUnsubscribe() {
+    this.randomNumberSubscription$.unsubscribe();
+  }
+  resetStreams() {
+    this.numberSubscriptionUnsubscribe();
+    this.randomNumberSubscriptionUnsubscribe();
+    this.numbers = [];
+    this.randomNumbers = [];
+    this.initStreams();
+  }
 }
