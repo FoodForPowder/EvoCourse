e3f4f54cd4cc5368afea18b2bfcc7c06209e2d60
FoodForPowder <zaharkin.artur@yandex.ru>
2025-04-20 13:09:46 +0700

    added 2.6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ComponentRef,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
+import { TableComponent } from './table/table.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  @ViewChild('table', { read: ViewContainerRef })
+  private viewRef!: ViewContainerRef;
+  private componentRef!: ComponentRef<TableComponent>;
   title = 'EvoCourse';
+
+  addComponent() {
+    this.viewRef.clear();
+    this.componentRef = this.viewRef.createComponent(TableComponent);
+  }
+  deleteComponent() {
+    this.viewRef.clear();
+  }
 }
